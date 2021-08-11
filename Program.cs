12d28e5764da629b1dc207c8dda667f05e8fa13b
79005 <79005@DESKTOP-ESBC4J0>
2021-08-11 18:19:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace homework4_Jakovlev
+{
+    class Program //Яковлев Сергей 
+    //Tusk 1
+    {
+        /// <summary>Метод подсчёта количества пар элементов массива, в которых хотя бы одно число делится на 3</summary>
+        /// <param name="array">Массив элементов</param>
+        /// <param name="length">Длинна массива элементов</param>
+        /// <returns></returns>
+        static int GetNumberOfPairs(int[] array, int length)
+        {
+            int amountOfPairs = 0;
+            for (int i = 0; i < length - 1; i++)
+            {
+                if (array[i] % 3 == 0 || array[i + 1] % 3 == 0)
+                {
+                    amountOfPairs++;
+                }
+            }
+            return amountOfPairs;
+        }
+
+        static void Main(string[] args)
+        {
+            const int arrayLength = 20;
+            int[] myArray = new int[arrayLength];
+            Random random = new Random();
+            int result;
+
+            Console.WriteLine("Вас приветствует программа подсчёта пар элементов, в которых хотя бы одно число делится на 3.");
+            Console.Write("\nВ следующем массиве [ ");
+            for (int i = 0; i < arrayLength; i++)
+            {
+                myArray[i] = random.Next(-10000, 10001);
+                Console.Write(myArray[i] + ", ");
+            }
+            Console.WriteLine("\b\b ]\n");
+
+            result = GetNumberOfPairs(myArray, arrayLength);
+
+            Console.WriteLine($"Количество пар: {result}");
+
+            Console.ReadKey();
+        }
+    }
+
+
+
+
+
+
+}
+
